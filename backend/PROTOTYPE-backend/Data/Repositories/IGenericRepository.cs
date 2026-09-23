42253b87a48e3a636aa7b8af597f0e3025cae297
KecsMilan <kecskemeti.milan@diak.szbi-pg.hu>
2026-09-23 10:12:10 +0200

    Added: new folder, files
    new folder:
    Repositories
    new files:
    GenericRepository.cs
    IGenericRepository.cs

---
+++ backend/PROTOTYPE-backend/Data/Repositories/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace PROTOTYPE_backend.Data.Repositories;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(int id);
+    Task<IEnumerable<T>> FindAsync(
+        Expression<Func<T, bool>> predicate,
+        Func<IQueryable<T>, IQueryable<T>>? include = null
+    );
+    Task<T?> FirstOrDefault(
+        Expression<Func<T, bool>> predicate,
+        Func<IQueryable<T>, IQueryable<T>>? include = null
+    );
+    Task AddAsync(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
--- backend/PROTOTYPE-backend/obj/Debug/net8.0/PROTOTYPE-backend.AssemblyInfo.cs
+++ backend/PROTOTYPE-backend/obj/Debug/net8.0/PROTOTYPE-backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROTOTYPE-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb414b461eb37f7dbd1fc268de21d711fad723d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d21c5a1ee8a54d80ffb6401a08063055ad9d48c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROTOTYPE-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROTOTYPE-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
